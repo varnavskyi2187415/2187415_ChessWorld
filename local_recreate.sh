4a40046ff27d0df29b4f8c27860aaa7be9adcd62
Andrew Vozniuk <lymphbizkit@gmail.com>
2024-11-02 13:00:01 +0100

    MySQL and PhpMyAdmin is working

--- local_recreate.sh
+++ local_recreate.sh
@@ -3,5 +3,5 @@
 GREEN='\033[0;32m'
 NC='\033[0m' 
 
-docker compose up -d --build
+docker compose -f docker-compose.yml -f docker-compose.local.yml up -d --build
 echo -e "=======================${GREEN}FINISHED!${NC}==========================="
