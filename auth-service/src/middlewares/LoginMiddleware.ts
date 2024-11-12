405a85988756514e7d411c290f4ef9d1139935f8
Andrew Vozniuk <lymphbizkit@gmail.com>
2024-11-12 23:44:02 +0100

    Access token generation is done.

--- auth-service/src/middlewares/LoginMiddleware.ts
+++ auth-service/src/middlewares/LoginMiddleware.ts
@@ -1,7 +1,18 @@
-export default module.exports = (req: any, res: any, next: any) => {
+import {User} from "../entity/User";
+import readCredentials from "../utils/getUserByEmail";
+import bcrypt from "bcrypt";
+
+export default  module.exports = async (req: any, res: any, next: any) => {
     const {email, password} = req.body;
-    if (!email && !password){
-        return res.status(400).json({ message: 'Invalid credentials' });
+
+    if (!(email &&password)){
+        return res.status(400).json({ message: 'Bad request' });
+    }
+
+    const user: User | null = await readCredentials(email);
+    const passwordMatched = await bcrypt.compare(password, <string>user?.password);
+    if (!passwordMatched) {
+        return res.status(400).json({ message: 'Bad request', isPasswordMatch: passwordMatched });
     }
 
     next();
