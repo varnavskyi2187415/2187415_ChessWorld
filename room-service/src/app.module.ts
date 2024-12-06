570d8e3af32199e6f76937727561b5f52a32d40b
Andrew Vozniuk <lymphbizkit@gmail.com>
2024-12-06 21:34:17 +0100

    Room service pakages installed

--- room-service/src/app.module.ts
+++ room-service/src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomModule } from './room/room.module';
 import { LoggingMiddleware } from './logging.middleware';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [RoomModule],
+  imports: [
+    RoomModule,
+    ClientsModule.register([
+      {
+        name: 'ROOM_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://rabbitmq:5672'],
+          queue: 'room-queue',
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
