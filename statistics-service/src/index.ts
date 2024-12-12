8eb9503b611ad8dc98723655c481381fa0e2848f
Andrew Vozniuk <lymphbizkit@gmail.com>
2024-12-12 11:15:25 +0100

    Started working on shearch opponent logic

--- statistics-service/src/index.ts
+++ statistics-service/src/index.ts
@@ -5,6 +5,7 @@ import RabbitMQMessageRouter from './routes/RabbitMQMessageRoutes';
 import { API_PREFIX } from './config';
 import {handleRabbitMQMessage} from "./rabbitmq/handlers/handle.rabbitmq.message";
 import {handleFindGameMessage} from "./rabbitmq/handlers/hadle.find.game.message";
+import {connectRedis} from "./services/redis.service";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,6 +18,7 @@ async function startServer() {
         await AppDataSource.initialize();
 
         await connectRabbitMQClient();
+        await connectRedis();
 
         subscribeToQueue('statistics-queue', handleRabbitMQMessage);
         subscribeToQueue('find-game-queue', handleFindGameMessage);
--- statistics-service/src/rabbitmq/handlers/hadle.find.game.message.ts
+++ statistics-service/src/rabbitmq/handlers/hadle.find.game.message.ts
@@ -1,7 +1,16 @@
 import {UserDto} from "../../dto/user.dto";
+import {getRedisClient} from "../../services/redis.service";
 
-export const handleFindGameMessage = (message: any) => {
+export const handleFindGameMessage = async (message: any) => {
     const userDto = new UserDto(message.data.email, message.data.userId);
+    const client = getRedisClient();
 
+    await client.set(userDto.userId, userDto.email);
+    const keys = await client.keys('*');
+    const values = await client.mGet(keys);
 
+    console.log(values[0]);
+    const value = await client.get(userDto.userId);
+
+    console.log('Retrieved value from Redis:', value);
 };
