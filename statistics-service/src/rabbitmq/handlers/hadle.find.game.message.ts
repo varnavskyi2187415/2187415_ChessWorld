1fea9599b570b9406538088bb7b06b3f365a7487
Andrii Vozniuk <lymphbizkit@gmail.com>
2025-01-07 22:28:18 +0100

    Last func from Andrew

--- statistics-service/src/rabbitmq/handlers/hadle.find.game.message.ts
+++ statistics-service/src/rabbitmq/handlers/hadle.find.game.message.ts
@@ -1,16 +1,34 @@
-import {UserDto} from "../../dto/user.dto";
-import {getRedisClient} from "../../services/redis.service";
+import { UserDto } from "../../dto/user.dto";
+import { getRedisClient } from "../../services/redis.service";
+import { forwardMessageToRoomQueue } from "../rabbitmq.service";
 
 export const handleFindGameMessage = async (message: any) => {
     const userDto = new UserDto(message.data.email, message.data.userId);
     const client = getRedisClient();
-
+    console.log(message);
     await client.set(userDto.userId, userDto.email);
+
     const keys = await client.keys('*');
     const values = await client.mGet(keys);
 
-    console.log(values[0]);
-    const value = await client.get(userDto.userId);
+    console.log('All values in Redis:', values);
 
+    if (values.length >= 2) {
+        const roomMessage = {
+            pattern: "room-queue",
+            data: {
+                whiteId: keys[0],
+                blackId: keys[1],
+                timeControl: '10|10'
+            }
+        };
+
+        forwardMessageToRoomQueue(roomMessage);
+
+        await client.del(keys[0]);
+        await client.del(keys[1]);
+    }
+
+    const value = await client.get(userDto.userId);
     console.log('Retrieved value from Redis:', value);
 };
